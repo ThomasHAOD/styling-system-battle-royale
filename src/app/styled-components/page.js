279aa617bad47c8d23240fb360737e4ea044e280
Tam O'Donnell <47383699+ThomasHAOD@users.noreply.github.com>
2023-07-27 17:59:57 +0100

    initial commit

---
+++ src/app/styled-components/page.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+const StyledLogo = styled.img`
+  height: 20px;
+`;
+const StyledProfileImage = styled.img`
+  height: 32px;
+`;
+
+const Page = () => {
+  const navigation = ['Company', 'Marketplace', 'Features', 'Team', 'Contact'];
+
+  const navItems = navigation.map((item) => <div key={item}>{item}</div>);
+
+  const users = [
+    {
+      name: 'Alice',
+      role: '10x Dev',
+      profileImg:
+        'https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png',
+    },
+    {
+      name: 'Bob',
+      role: 'Analyst Extrordinaire',
+      profileImg:
+        'https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png',
+    },
+    {
+      name: 'Charlie',
+      role: 'UX Whizkid',
+      profileImg:
+        'https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png',
+    },
+  ];
+
+  const userComponents = users.map(({ name, role, profileImg }) => {
+    return (
+      <div key={name}>
+        <StyledLogo src={profileImg} />
+        <h3>{name}</h3>
+        <p>{role}</p>
+        <div>
+          <button>Add Friend</button>
+          <button>Message</button>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <nav>
+        <div>{navItems}</div>
+        <StyledProfileImage src='https://app.jobvite.com/logo/3522_ANDLOGORGBONWHITE_1493728768591_Company.png' />
+      </nav>
+      <section>
+        <h2>We invest in the world&apos;s potential</h2>
+        <p>
+          Here at AND we focus on markets where technology, innovation, and
+          capital can unlock long-term value and drive economic growth.
+        </p>
+      </section>
+      <div>{userComponents}</div>
+    </div>
+  );
+};
+
+export default Page;
